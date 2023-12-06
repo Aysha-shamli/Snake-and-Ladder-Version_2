58a4ecb0fe824ddf29f6b60f6fa63d247e19c825
Aysha-shamli <ayshashamli1999@gmail.com>
2023-12-06 20:04:51 +0530

    Initialized the snake and ladder game program

---
+++ SnakeAndLadderGame.java
@@ -0,0 +1,12 @@
+package snakeAndLadder;
+
+public class SnakeAndLadderGame {
+    public static void main(String[] args) {
+        int playerPosition = 0;
+        int BoardSize = 100;
+        System.out.println("The game start at position: "+playerPosition);
+        System.out.println("The size of the game board is: "+BoardSize);
+        System.out.println("The Ladder Positions on GameBoard: [2, 8, 20, 32, 41, 74, 82, 85]");
+        System.out.println("The Snake Positions on GameBoard: [29, 38, 47, 53, 62, 86, 92, 97]");
+    }
+}
