036179617188d9b20d6b6292ebdb9a4b1b0c8e35
Aysha-shamli <ayshashamli1999@gmail.com>
2023-12-06 20:38:38 +0530

    modified the program to check with options:Noplay,snake,ladder

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -6,17 +6,16 @@ public class SnakeAndLadderGame {
         int BoardSize = 100;
         System.out.println("The game start at position: "+playerPosition);
         System.out.println("The size of the game board is: "+BoardSize);
-        System.out.println("The Ladder Positions on GameBoard: [2, 8, 20, 32, 41, 74, 82, 85]");
-        System.out.println("The Snake Positions on GameBoard: [29, 38, 47, 53, 62, 86, 92, 97]");
 
         playGame(playerPosition, BoardSize);
     }
     static void playGame(int playerPosition,int BoardSize) {
         while (playerPosition < BoardSize) {
             int diceRoll = rollDice();
-            playerPosition = playerPosition + diceRoll;
+            int option = checkOption();
+            playerPosition = movePlayer(playerPosition, option, diceRoll);
             System.out.println("After rolling a " + diceRoll + ", the player is at position " + playerPosition);
-
+            
             if (playerPosition == BoardSize) {
                 System.out.println("Congratulations! You won the Game!!!");
                 break;
@@ -26,5 +25,26 @@ public class SnakeAndLadderGame {
     static int rollDice() {
         Random random = new Random();
         return random.nextInt(6) + 1; // Generates a random number between 1 and 6
+    }
+    static int checkOption() {
+        Random random = new Random();
+        return random.nextInt(3); // Generates a random number between 0 and 2 for No Play, Ladder, or Snake
+    }
+    static int movePlayer(int playerPosition, int option, int diceRoll) {
+        switch (option) {
+            case 0:
+                System.out.println("No Play. Player stays at the same position.");
+                break;
+            case 1:
+                System.out.println("Ladder! Player moves ahead by " + diceRoll + " positions.");
+                return playerPosition + diceRoll;
+            case 2:
+                System.out.println("Snake! Player moves behind by " + diceRoll + " positions.");
+                return playerPosition - diceRoll;
+            default:
+                System.out.println("Invalid option.");
+                return playerPosition;
+        }
+        return playerPosition;
     }
 }
